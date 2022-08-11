29205be342c03145ca0052da0f6bd5bfc56a0ee0
Sunny Lim <limhss@gmail.com>
2022-08-11 18:17:10 +0800

    S

--- pages/1_Empty.py
+++ pages/1_Empty.py
@@ -16,7 +16,7 @@ else:
     res = pledges.fetch(query={"player" : "Mason Bain"})
     max_index = res.count - 1
     st.subheader("Your pledges :")
-    display = [y.pop('card_id') for y in [x.pop('key') for x in res.items]]
+    display = [x.pop('key').pop('card_id') for x in res.items]
     with st.expander("Show Pledge Details"):
 
         st.dataframe(display)
