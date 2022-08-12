e4127da84fa92d8318aa848402f7a59e457ca114
Sunny Lim <limhss@gmail.com>
2022-08-12 14:57:54 +0800

    S

--- pages/1_Empty.py
+++ pages/1_Empty.py
@@ -24,7 +24,7 @@ else:
             np.random.randn(50, 3),
             columns=["a", "b", "c"])
         chart_data= pd.DataFrame(res.items).groupby('points')['card_id'].count()
-        st.bar_chart(chart_data, 'points', 'card_id')
+        st.bar_chart(chart_data, 'points', 'card_id', use_container_width=True)
     with st.expander("Show Pledge Details"):
         st.write(df)
         with st.form("Pick the record by its index to display",clear_on_submit=True):
