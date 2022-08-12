6db355b5683e6b7c9b6aedc5b85b1c9e1e80a8d7
Sunny Lim <limhss@gmail.com>
2022-08-12 18:32:24 +0800

    S

--- pages/1_Empty.py
+++ pages/1_Empty.py
@@ -24,7 +24,7 @@ else:
         chart_data = pd.DataFrame(
             np.random.randn(50, 3),
             columns=["a", "b", "c"])
-        df_c = pd.DataFrame(res.items)['points'].value_counts()
+        df_c = pd.DataFrame(pd.DataFrame(res.items)['points'].value_counts()).column['point', 'count']
         st.write(df_c.head())
         #chart = alt.Chart(df_c).mark_bar().encode(
             #x='point',
