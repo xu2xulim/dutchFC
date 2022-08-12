007ce42dc5872b5bfb84815c5396803e7901a8ef
Sunny Lim <limhss@gmail.com>
2022-08-12 18:15:59 +0800

    S

--- pages/1_Empty.py
+++ pages/1_Empty.py
@@ -24,7 +24,7 @@ else:
         chart_data = pd.DataFrame(
             np.random.randn(50, 3),
             columns=["a", "b", "c"])
-        df_c = pd.DataFrame(res.items).groupby('points')['pledger'].count()
+        df_c = pd.DataFrame(res.items).groupby('points')['points'].count()
         chart = alt.Chart(df_c).mark_bar().encode(
             x='points',
             y='pledger'
