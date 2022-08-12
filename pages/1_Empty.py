94c2bcc7e78f461257d4ffda59228cb541a9093b
Sunny Lim <limhss@gmail.com>
2022-08-12 19:44:35 +0800

    S

--- pages/1_Empty.py
+++ pages/1_Empty.py
@@ -38,7 +38,8 @@ else:
                 st.write("Phone :", res.items[show_index]['phone'])
                 st.write("Points :", res.items[show_index]['points'])
                 #st.write("Pledge Per Point :", res.items[show_index]['pledgepp'])
-                st.write("Amount :", res.items[show_index]['amount'])
+                st.write("Maximum Amount :", res.items[show_index]['amount'])
+                #st.write("Maximum Amount :", res.items[show_index]['max_amt'])
                 st.write("Phone :", res.items[show_index]['phone'])
                 try :
                     st.write("Status :", res.items[show_index]['status'])
