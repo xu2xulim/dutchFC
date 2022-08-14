fa4789cb970f89c54e0f004cc716ba67dfc20a6a
Sunny Lim <limhss@gmail.com>
2022-08-14 14:10:21 +0800

    S

--- pages/2_Past Season Collection.py
+++ pages/2_Past Season Collection.py
@@ -23,7 +23,7 @@ else:
     if refresh :
         st.experimental_rerun()
     with st.expander("Show Pledge Details"):
-        st.session_state['update'] = "False"
+        #st.session_state['update'] = "False"
         action = "No"
         with st.form("Pick the record by its index to display",clear_on_submit=True):
             show_index = st.number_input("Index", min_value=0, max_value=max_index, step=1)
